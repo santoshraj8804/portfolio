f18e5b8ecebc97b66b568a289616e29eec301eb8
Santosh raj <santoshrajgupta7366@gmail.com>
2025-09-18 17:03:09 +0530

    solved resume download problem

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -43,9 +43,8 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="assets/resume.jpg"
-            target="_blank"
-            rel="noopener noreferrer"
+            href='/resume.jpg'
+            download
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
             style={{
               background: 'linear-gradient(90deg, #8245ec, #a855f7)',
